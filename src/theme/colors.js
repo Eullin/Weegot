580c2dd228c2fa4a52236facea7c286dddb3c8e8
Roger Andersen <ra@bjerk.io>
2019-10-10 18:29:54 +0200

    feat: add a button 👌

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,6 +1,6 @@
 const colors = {
     transparent: "transparent", // 0
-    brandAutos: "#D85C72", // 1
+    brandAutos: "#cc4e63", // 1
     brandHelp: "#2087A0", // 2
     textColor: "#292929", // 3
     lightTextColor: "#757575", // 4
