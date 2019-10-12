eac38a442d229399d90437b805a897867f493863
“Eullin <eullinch@gmail.com>
2019-10-12 18:29:19 +0200

    fix: layout.css file remove

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import { ThemeProvider } from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import { theme } from "../theme/index"
 import GlobalStyle from "../styles/Global.js"
@@ -12,12 +11,7 @@ const IndexPage = () => (
     <GlobalStyle />
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
+      <h1>Homepage</h1>
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   </ThemeProvider>
