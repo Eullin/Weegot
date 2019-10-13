cc7f43c6ca06b279ecd03441c60517b55c9def97
Roger Andersen <hexjelly@hexjelly.com>
2019-10-13 10:44:24 +0200

    refactor: change to use Typescript files

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -1,9 +1,6 @@
 import { createGlobalStyle } from "styled-components"
 
 const GlobalStyles = createGlobalStyle`
-
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap');;
-
   *,
   *::after,
   *::before {
@@ -18,7 +15,7 @@ const GlobalStyles = createGlobalStyle`
 
   body {
     box-sizing: border-box;
-    font-family: 'Open Sans', sans-serif;
+    font-family: 'Roboto', sans-serif;
   }
 `
 
