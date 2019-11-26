1157640401f5a8ff3e5814660700615d4c5bf853
Eullin Chang <ec@Simens-iMac.local>
2019-11-26 08:49:01 +0100

    fix: merge issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
 
-<<<<<<< Updated upstream
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Homepage from '../containers/Homepage'
-=======
-import Layout from "../../components/layout"
-import SEO from "../../components/seo"
-import Button from "../../components/button"
-import Hero from "./components/Hero"
->>>>>>> Stashed changes
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-<<<<<<< Updated upstream
     <Homepage />
-=======
-    <Hero />
-    <Button>Test test</Button>
->>>>>>> Stashed changes
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
