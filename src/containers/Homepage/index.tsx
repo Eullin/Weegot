b72bbaf8c97618a50b6e0a59ab813605057b7d8c
“Eullin <eullinch@gmail.com>
2019-12-30 22:31:28 +0100

    WIP: progress section

--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -1,21 +1,28 @@
 import React from "react"
 import Img from "gatsby-image"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import Heading from "../../components/Heading"
-import Paragraph from '../../components/Paragraph'
+import Paragraph from "../../components/Paragraph"
 import Button from "../../components/button"
-import TextArea from "../../components/TextArea"
-import {MainContainer} from "../../components/MainContainer"
-import { Container, ButtonWrapp, ProcessWrap, CtaLink, CardContainer, IMG } from "./homepage.styles"
+import { MainContainer } from "../../components/MainContainer"
+import {
+  Container,
+  ButtonWrapp,
+  ProcessWrap,
+  CtaLink,
+  CardContainer,
+  IMG,
+  Icon,
+} from "./homepage.styles"
 import PlainCard from "../../components/PlainCard"
-
 
 const Homepage = () => {
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "car.png" }) {
-        childImageSharp {
+      allImageSharp {
+        nodes {
           fluid(maxWidth: 300) {
+            originalName
             ...GatsbyImageSharpFluid
           }
         }
@@ -26,15 +33,16 @@ const Homepage = () => {
     <>
       <MainContainer>
         <PlainCard bg="secondaryBrand" p={3}>
-          <Container flexDirection={['column','row']} justifyContent="space-between" >
+          <Container
+            flexDirection={["column", "row"]}
+            justifyContent="space-between"
+          >
             <Heading as="h1" color="white" pl={3} width={1}>
-              Ahorra tiempo y dinero 
-              en la compra de tu 
-              próximo auto
+              Ahorra tiempo y dinero en la compra de tu próximo auto
             </Heading>
             <div style={{ width: "100%", flexGrow: 1 }}>
               <IMG
-                fluid={data.file.childImageSharp.fluid}
+                fluid={data.allImageSharp.nodes[3].fluid}
                 position="relative"
                 bottom={[6, 7, 8, 9]}
               />
@@ -45,44 +53,52 @@ const Homepage = () => {
           <Button>Contáctanos</Button>
         </ButtonWrapp>
         <Paragraph m={4} mb={4} lineHeight="normalText">
-            Somos expertos al momento de negociar la compra y venta de
-            automóviles.​ Evita ser engañado por concesionarios sin principios
-            que, con el objetivo de lograr una venta, implementan estrategias
-            maliciosas que afectan tu pago inicial, tus cuotas y hasta tu
-            situación crediticia. Te garantizamos el mejor negocio posible según
-            tu situación.
+          Somos expertos al momento de negociar la compra y venta de
+          automóviles.​ Evita ser engañado por concesionarios sin principios
+          que, con el objetivo de lograr una venta, implementan estrategias
+          maliciosas que afectan tu pago inicial, tus cuotas y hasta tu
+          situación crediticia. Te garantizamos el mejor negocio posible según
+          tu situación.
         </Paragraph>
       </MainContainer>
       <ProcessWrap>
-      <Heading as="h2" textAlign="center" fontWeight={400} color="textColor"> Nuestro Proceso</Heading>
-        <CardContainer flexDirection={['column','row']}>
-        	<PlainCard bg="brandAutos" p={3}>
-        	  <Paragraph color="white" p={3}>
-        	  <Heading as="h2" mt={2} mb={2}>
-        	    Llena nuestra aplicación
-        	  </Heading>
-        	  Esta nos brindará toda la información necesaria para conocer tu situación y tus posibilidades
-        	</Paragraph>
-        	  <CtaLink to="/contacto">Comenzar ahora →</CtaLink>
-        	</PlainCard>
-        	<PlainCard bg="secondaryBrand" p={3}>
-        	<Paragraph color="white" p={3}>
-        	  <Heading as="h2" mt={2} mb={2}>
-        	    Llena nuestra aplicación
-        	  </Heading>
-        	  Esta nos brindará toda la información necesaria para conocer tu situación y tus posibilidades
-        	</Paragraph>
-        	   <CtaLink to="/contacto">Comenzar ahora →</CtaLink>
-        	</PlainCard>
-        	<PlainCard bg="brandAutos" p={3}>
-        	<Paragraph color="white" p={3}>
-        	  <Heading as="h2" mt={2} mb={2}>
-        	    Llena nuestra aplicación
-        	  </Heading>
-        	  Esta nos brindará toda la información necesaria para conocer tu situación y tus posibilidades
-        	</Paragraph>
-        	   <CtaLink to="/contacto">Comenzar ahora →</CtaLink>
-        	</PlainCard>
+        <Heading as="h2" textAlign="center" fontWeight={400} color="textColor">
+          Nuestro Proceso
+        </Heading>
+        <CardContainer flexDirection={["column", "row"]}>
+          <PlainCard p={3} textAlign={["center", "left"]}>
+            <Icon fluid={data.allImageSharp.nodes[6].fluid} />
+            <Heading as="h2" mt={2} mb={2}>
+              Asesoría
+            </Heading>
+            <Paragraph p={3}>
+              Esta nos brindará toda la información necesaria para conocer tu
+              situación y tus posibilidades
+            </Paragraph>
+            <CtaLink to="/contacto">Comenzar ahora →</CtaLink>
+          </PlainCard>
+          <PlainCard p={3} textAlign={["center", "left"]}>
+            <Paragraph p={3}>
+              <Icon fluid={data.allImageSharp.nodes[8].fluid} />
+              <Heading as="h2" mt={2} mb={2}>
+                Gestión
+              </Heading>
+              Esta nos brindará toda la información necesaria para conocer tu
+              situación y tus posibilidades
+            </Paragraph>
+            <CtaLink to="/contacto">Comenzar ahora →</CtaLink>
+          </PlainCard>
+          <PlainCard p={3} textAlign={["center", "left"]}>
+            <Paragraph p={3}>
+              <Icon fluid={data.allImageSharp.nodes[7].fluid} />
+              <Heading as="h2" mt={2} mb={2}>
+                Delivery
+              </Heading>
+              Esta nos brindará toda la información necesaria para conocer tu
+              situación y tus posibilidades
+            </Paragraph>
+            <CtaLink to="/contacto">Comenzar ahora →</CtaLink>
+          </PlainCard>
         </CardContainer>
       </ProcessWrap>
     </>
