4a46b2d6bf7300bd70cae23831b02d2ffe3bec25
Personal <personal@Eullins-MacBook-Pro.local>
2020-04-09 11:29:16 +0200

    netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,7 @@ module.exports = {
           },
         ],
       },
+      resolve: `gatsby-plugin-netlify`,
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
